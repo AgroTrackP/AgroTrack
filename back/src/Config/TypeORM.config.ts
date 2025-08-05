a0bf4fa974c17692ba1942d77883a13d8b1a3591
AFacundoOrtiz <urban12the@gmail.com>
2025-08-05 17:54:37 -0300

    typeorm configurado

---
+++ back/src/Config/TypeORM.config.ts
@@ -0,0 +1,28 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { config as dotenvConfig } from 'dotenv';
+import { registerAs } from '@nestjs/config';
+
+dotenvConfig({ path: '.env.development' });
+
+if (!process.env.DB_PORT) {
+  throw new Error('DB_PORT is not defined in the environment variables');
+}
+
+export const databaseConfig: DataSourceOptions = {
+  type: 'postgres',
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  synchronize: false,
+  logging: true,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/migrations/*.js'],
+};
+
+export default registerAs('typeorm', () => databaseConfig);
+
+export const connectionSource = new DataSource(
+  databaseConfig as DataSourceOptions,
+);
--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,7 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import TypeORMConfig from './Config/TypeORM.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceOptions } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [TypeORMConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const config = configService.get<DataSourceOptions>('typeorm');
+        return {
+          ...config,
+          autoLoadEntities: true,
+        };
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
