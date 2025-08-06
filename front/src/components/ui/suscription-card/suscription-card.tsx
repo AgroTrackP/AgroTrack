7dbef9aa4eb4119f305271a505ec13ad7d4426e2
esperanza <judithesperanza1983@gmail.com>
2025-08-06 14:39:27 -0500

    se empieza componente

---
+++ front/src/components/ui/suscription-card/suscription-card.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+interface ISuscription{
+    id: string,
+    plan: string,
+    price: number,
+    benefits: string[]
+}
+
+const SuscriptionCard = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default SuscriptionCard
