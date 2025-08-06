c6f0cab09ef05b45bb1bf18695dea7afada4dc68
JeisonGuasca <guascajeison@gmail.com>
2025-08-06 16:50:14 -0500

    auth/register

--- front/src/services/auth.ts
+++ front/src/services/auth.ts
@@ -1,3 +1,31 @@
-export const postRegister = async (data: any) => {
+"use server";
+import { RegisterUserDto } from "@/types";
+import { axiosApiBack } from "./utils";
 
-}
+const BASE_URL = "https://agrotrack-develop.onrender.com";
+
+export const postRegister = async (data: RegisterUserDto) => {
+    try {
+        const res = await axiosApiBack.post(`${BASE_URL}/register`, data);
+
+        if (!res.data) {
+            console.warn("Invalid post register data format", res.data);
+            return {
+                message: "Error al registrar el usuario",
+                errors: res.data,
+            };
+        }
+        return {
+            message: "Usuario registrado correctamente",
+        };
+        
+    } catch (e: unknown) {
+        if (e instanceof Error) {
+            console.warn("Error fetching post register", e.message);
+        }
+        return {
+            message: "Error al registrar el usuario",
+            errors: (e as Error).message || "Error desconocido",
+        };
+    }
+};
