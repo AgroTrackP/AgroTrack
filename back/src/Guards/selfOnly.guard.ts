bcef9a40f5c1e6bebeee828dba112e80d4e0c1f6
AFacundoOrtiz <urban12the@gmail.com>
2025-08-05 21:03:41 -0300

    Validación de acceso con id. Acceso de admin

---
+++ back/src/Guards/selfOnly.guard.ts
@@ -0,0 +1,31 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from 'src/Modules/Users/user.enum';
+import { AuthRequest } from 'src/Types/express';
+
+@Injectable()
+export class SelfOnlyGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const request: AuthRequest = context.switchToHttp().getRequest();
+    const user = request.user;
+    const userIdFromToken = request.user.sub;
+    const userIdFromParams = request.params.id;
+
+    const isAdmin = user.role === Role.Admin;
+
+    if (isAdmin || userIdFromToken === userIdFromParams) {
+      return true;
+    }
+
+    throw new ForbiddenException(
+      'You do not have permission to access this resource.',
+    );
+  }
+}
