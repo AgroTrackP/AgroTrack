4c3f0e2fb9cd2687048b15af64a6754f4f669cbf
esperanza <judithesperanza1983@gmail.com>
2025-08-07 15:11:54 -0500

    Se eliminan comentario que no son necesarios

--- front/src/app/(views)/(home)/page.tsx
+++ front/src/app/(views)/(home)/page.tsx
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 import SuscriptionList from "./components/suscription-list";
-=======
-// import Carrusel from "@/components/layout/Carousel";
->>>>>>> cc45ee3eda9822a800f21d09ff595acf35305c23
 
 export default function Home() {
   return (
     <>
-<<<<<<< HEAD
       <SuscriptionList/>
-=======
-      {/* <Carrusel/> */}
->>>>>>> cc45ee3eda9822a800f21d09ff595acf35305c23
     </>
   )
 }
--- front/src/types/index.ts
+++ front/src/types/index.ts
@@ -1,47 +1,3 @@
-// <<<<<<< HEAD
-// export const mockUsers = [
-//   {
-//     id: 1,
-//     name: "Juan Pérez",
-//     email: "juan.perez@example.com",
-//     password: "123456",
-//     address: "Calle Falsa 123, Lima",
-//     phone: "987654321",
-//   },
-//   {
-//     id: 2,
-//     name: "María Gómez",
-//     email: "maria.gomez@example.com",
-//     password: "password123",
-//     address: "Av. Las Palmeras 456, Arequipa",
-//     phone: "912345678",
-//   },
-//   {
-//     id: 3,
-//     name: "Carlos Fernández",
-//     email: "carlos.fernandez@example.com",
-//     password: "qwerty789",
-//     address: "Jr. Los Olivos 789, Cusco",
-//     phone: "999111222",
-//   },
-//   {
-//     id: 4,
-//     name: "Lucía Ramírez",
-//     email: "lucia.ramirez@example.com",
-//     password: "lucia1234",
-//     address: "Calle Central 321, Trujillo",
-//     phone: "944333222",
-//   },
-//   {
-//     id: 5,
-//     name: "Andrés Salazar",
-//     email: "andres.salazar@example.com",
-//     password: "andrespass",
-//     address: "Pasaje Sur 222, Piura",
-//     phone: "933444555",
-//   },
-// ];
-// =======
 export interface RegisterUserDto { //post
     name: string
     email: string
@@ -49,7 +5,6 @@ export interface RegisterUserDto { //post
     address: string
     phone: string
 }
-<<<<<<< HEAD
 
 export interface ISuscription{
     id: string,
@@ -57,6 +12,3 @@ export interface ISuscription{
     price: number,
     benefits: string[]
 }
-=======
-// >>>>>>> 7238459bef4cd53d386acdb96d6a730ccfd1c2be
->>>>>>> cc45ee3eda9822a800f21d09ff595acf35305c23
