dc833c6a83aeee9ccf47d987d3ddb270fe7c2dfe
Jeison Guasca <guascajeison@gmail.com>
2025-08-07 12:21:17 -0500

    remove dark styles

--- front/src/app/(static)/(auth)/Register/components/registerForm.tsx
+++ front/src/app/(static)/(auth)/Register/components/registerForm.tsx
@@ -12,7 +12,7 @@ import { postRegister } from "@/services/auth";
 //Validaciones
 const RegisterSchema = yup.object().shape({
     email: yup.string().email('Invalid email').required('Required'),
-        password: yup.string()
+    password: yup.string()
         .min(6, 'La contraseña debe tener al menos 6 caracteres')
         .required('Campo requerido'),
     confirmPassword: yup.string()
@@ -29,20 +29,20 @@ interface RegisterUserForm extends RegisterUserDto {
     confirmPassword: string;
 };
 
-const RegisterForm = () => { 
+const RegisterForm = () => {
     const [showPassword, setShowPassword] = React.useState(false);
-        const handleShowPassword = () => {
+    const handleShowPassword = () => {
         setShowPassword((prev) => !prev);
     };
-    
+
     const initialValues: RegisterUserForm = {
-        email: '', 
-        password: '', 
-        confirmPassword: '' , 
-        phone: "", 
-        address:"", 
-        name:""
-        
+        email: '',
+        password: '',
+        confirmPassword: '',
+        phone: "",
+        address: "",
+        name: ""
+
         //harcodeo
         // name: "Juan Pérez",
         // email: "juan.perez@example.com",
@@ -51,134 +51,134 @@ const RegisterForm = () => {
         // phone: "+57 3104567890",
         // address: "Calle 123 #45-67, Bogotá, Colombia"
     }
-        const togglePasswordVisibility = () => {
+    const togglePasswordVisibility = () => {
         setShowPassword(prev => !prev);
     };
-    
-    
-    const handleOnSubmit = async(values:RegisterUserForm, { setSubmitting }: any) => { 
-    const {confirmPassword, ...data} = values;
+
+
+    const handleOnSubmit = async (values: RegisterUserForm, { setSubmitting }: any) => {
+        const { confirmPassword, ...data } = values;
         try {
-    const res = await postRegister(data);
+            const res = await postRegister(data);
 
 
-//     if(res.errors) {
-//         return alert(" Error al registrar el usuario");
-// }
+            //     if(res.errors) {
+            //         return alert(" Error al registrar el usuario");
+            // }
 
-    alert("Usuario registrado correctamente");
-    
-    }catch(e) {
-        alert("Error al registrar el usuario");
-    
-    }finally {
-    setSubmitting(false);
-    
+            alert("Usuario registrado correctamente");
+
+        } catch (e) {
+            alert("Error al registrar el usuario");
+
+        } finally {
+            setSubmitting(false);
+
+        }
     }
-}
 
     return (
         <div>
-        <Formik
-    validationSchema={RegisterSchema}
-    initialValues= {initialValues}
-    onSubmit={handleOnSubmit}
-    
-    >
-    {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-        /* and other goodies */
-    }) => (
-        <form onSubmit={handleSubmit}>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <Formik
+                validationSchema={RegisterSchema}
+                initialValues={initialValues}
+                onSubmit={handleOnSubmit}
 
-        <Input
-            label="Nombre"
-            id="name"
-            type="text"
-            name="name"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.name}
-            error={errors.name && touched.name ? errors.name : ""}
-        />
-        <Input
-            label="Email"
-            id="email"
-            type="text"
-            name="email"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.email}
-            error={errors.email && touched.email ? errors.email : ""}
-        />
-        <Input className="mb-4"
-            label="Contraseña"
-            id="password"
-            type={showPassword ? "text" : "password"}
-            name="password"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.password}
-            error={errors.password && touched.password ? errors.password : ""}
-            children={<div onClick={handleShowPassword}>
-                {showPassword ? <FaEyeSlash /> : <FaEye />}
-                </div>
-                
-            }
-        />
+            >
+                {({
+                    values,
+                    errors,
+                    touched,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                    isSubmitting,
+                    /* and other goodies */
+                }) => (
+                    <form onSubmit={handleSubmit}>
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
 
-        <Input className="mb-4"
-            label="Confirmar contraseña"
-            id="confirmPassword"
-            type={showPassword ? "text" : "password"} 
-            name="confirmPassword"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.confirmPassword}
-            error={errors.confirmPassword && touched.confirmPassword ? errors.confirmPassword : ""}
-            children ={<div onClick={handleShowPassword}>
-                {showPassword ? <FaEyeSlash /> : <FaEye />}
-                </div>
-            }
-        />
-        <Input
-            label="Telefono"
-            id="phone"
-            type="text"
-            name="phone"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.phone}
-            error={errors.phone && touched.phone ? errors.phone : ""}
-        />
-        <Input
-            label="Dirección"
-            id="address"
-            type="text"
-            name="address"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.address}
-            error={errors.address && touched.address ? errors.address : ""}
-        />
+                            <Input
+                                label="Nombre"
+                                id="name"
+                                type="text"
+                                name="name"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.name}
+                                error={errors.name && touched.name ? errors.name : ""}
+                            />
+                            <Input
+                                label="Email"
+                                id="email"
+                                type="text"
+                                name="email"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.email}
+                                error={errors.email && touched.email ? errors.email : ""}
+                            />
+                            <Input className="mb-4"
+                                label="Contraseña"
+                                id="password"
+                                type={showPassword ? "text" : "password"}
+                                name="password"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.password}
+                                error={errors.password && touched.password ? errors.password : ""}
+                                children={<div onClick={handleShowPassword}>
+                                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                                </div>
+
+                                }
+                            />
+
+                            <Input className="mb-4"
+                                label="Confirmar contraseña"
+                                id="confirmPassword"
+                                type={showPassword ? "text" : "password"}
+                                name="confirmPassword"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.confirmPassword}
+                                error={errors.confirmPassword && touched.confirmPassword ? errors.confirmPassword : ""}
+                                children={<div onClick={handleShowPassword}>
+                                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                                </div>
+                                }
+                            />
+                            <Input
+                                label="Telefono"
+                                id="phone"
+                                type="text"
+                                name="phone"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.phone}
+                                error={errors.phone && touched.phone ? errors.phone : ""}
+                            />
+                            <Input
+                                label="Dirección"
+                                id="address"
+                                type="text"
+                                name="address"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.address}
+                                error={errors.address && touched.address ? errors.address : ""}
+                            />
+                        </div>
+                        <Button
+                            type="submit"
+                            disabled={isSubmitting}
+                            label="Registrarse"
+                            className="w-full"
+                        />
+                    </form>
+                )}
+            </Formik>
         </div>
-            <Button 
-            type="submit" 
-            disabled={isSubmitting}               
-            label="Registrarse"
-            className="w-full"
-        />
-        </form>
-    )}
-    </Formik>
-    </div>
     )
 }
 export default RegisterForm
