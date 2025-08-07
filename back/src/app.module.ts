314927c76d48180eb066655714654848dd2d75f9
FStevenson91 <felstefer28791@gmail.com>
2025-08-07 03:16:50 -0400

    arreglo imports en appmodule, limpie conflictos por hacer npm run start:dev para probar swagger

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -3,13 +3,10 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import TypeORMConfig from './Config/TypeORM.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSourceOptions } from 'typeorm';
-<<<<<<< HEAD
 import { CloudinaryModule } from './Modules/Cloudinary/cloudinary.module';
-=======
 import { AuthModule } from './Modules/Auth/auth.module';
 import { jwtConfig } from './Config/JWT.config';
 import { JwtModule } from '@nestjs/jwt';
->>>>>>> f28435d8675becd019da55471989a8579aded7c5
 
 @Module({
   imports: [
@@ -27,12 +24,9 @@ import { JwtModule } from '@nestjs/jwt';
         };
       },
     }),
-<<<<<<< HEAD
     CloudinaryModule,
-=======
     AuthModule,
     JwtModule.register(jwtConfig),
->>>>>>> f28435d8675becd019da55471989a8579aded7c5
   ],
   controllers: [],
   providers: [],
