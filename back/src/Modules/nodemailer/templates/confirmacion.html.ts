a598109af716eced33ab24f21ff94d3c6a42e7ad
FStevenson91 <felstefer28791@gmail.com>
2025-08-10 23:24:16 -0400

    ajustes en url confirmacion

--- back/src/Modules/nodemailer/templates/confirmacion.html.ts
+++ back/src/Modules/nodemailer/templates/confirmacion.html.ts
@@ -7,9 +7,12 @@ export const confirmationTemplate = (option: {
   <h1>¡Hola ${name}!</h1>
 <p>Gracias por registrarte en Agrotrack.</p>
 <p>Por favor confirma tu cuenta haciendo clic en el enlace:</p>
-<a href="https://agrotrack-develop.onrender.com/api/confirmar?email=${email}">Confirmar Cuenta</a>
+<a href="https://agrotrack-develop.onrender.com/auth/confirmar/${email}">Confirmar Cuenta</a>
 <p>Si no te registraste, ignora este mensaje.</p>
 <p>Saludos,<br>El equipo de Agrotrack</p>
   
   </div>`;
 };
+
+//en auth confirmacion y recuperacion de contraseña van aca
+//crear endpoint GET confirmacion, q reciba parametro email y confirme el usuario
