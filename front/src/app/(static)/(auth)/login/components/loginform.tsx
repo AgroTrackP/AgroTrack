afa2b71b3fa165e2925d6d53c9cec100eb5137e8
PauloCLG09 <pauce01@gmail.com>
2025-08-10 21:46:36 -0500

    formulario de terreno

--- front/src/app/(static)/(auth)/login/components/loginform.tsx
+++ front/src/app/(static)/(auth)/login/components/loginform.tsx
@@ -33,7 +33,7 @@ export default function LoginForm() {
         }
 
         try {
-            const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
+            const res = await fetch(`https://agrotrack-develop.onrender.com//auth/login`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json"},
                 body: JSON.stringify(form)
