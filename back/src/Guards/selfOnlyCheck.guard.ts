ff783b3d1e73edd4cad1f1baf8169c25b2a0530e
AFacundoOrtiz <urban12the@gmail.com>
2025-08-15 02:25:57 -0300

    cambios pequeños

--- back/src/Guards/selfOnlyCheck.guard.ts
+++ back/src/Guards/selfOnlyCheck.guard.ts
@@ -5,26 +5,20 @@ import {
   Injectable,
 } from '@nestjs/common';
 import { Users } from '../Modules/Users/entities/user.entity';
-import { Role } from '../Modules/Users/user.enum';
 import { CreateCheckoutSessionDto } from 'src/Modules/Stripe/dtos/createCheckoutSession.dto';
 
 @Injectable()
 export class SelfOnlyCheckGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
-    // ✅ Le decimos a TypeScript que 'user' es del tipo 'Users'
+
     const user: Users = request.user;
+
     const userIdFromToken = user.id;
 
-    // --- CAMBIO CLAVE AQUÍ ---
-    // Obtener el ID del usuario del cuerpo de la solicitud
     const { userId: userIdFromDto } = request.body as CreateCheckoutSessionDto;
-    // -------------------------
 
-    // Ahora 'user.id' no dará un error
-    const isAdmin = user.role === Role.Admin;
-
-    if (isAdmin || userIdFromToken === userIdFromDto) {
+    if (userIdFromToken === userIdFromDto) {
       return true;
     }
 
