d20413873caebf1bd91fea38cb4917266671a589
PauloCLG09 <pauce01@gmail.com>
2025-08-18 16:03:17 -0500

    commit

--- front/src/context/landContext.tsx
+++ front/src/context/landContext.tsx
@@ -49,7 +49,7 @@ export const LandProvider = ({ children }: { children: ReactNode }) => {
       console.log(newLand);
 
 
-      setLands((prevLands) => [...prevLands, newLand as LandData]);
+      setLands((prevLands) => [...prevLands, newLand as unknown as LandData]);
 
      
       
