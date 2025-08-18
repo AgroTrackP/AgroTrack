bcf9eaad0dc4f098e0282b86d6c4ef18b3de16a6
AgroTrackP <agrotrackproject@gmail.com>
2025-08-18 00:07:42 -0300

    Update Providers.tsx

--- front/src/app/Providers.tsx
+++ front/src/app/Providers.tsx
@@ -1,35 +1,30 @@
+// src/app/Providers.tsx
 "use client";
 
 import { AuthProvider } from "@/context/authContext";
 import { Auth0Provider } from "@auth0/auth0-react";
-
 import { LandProvider } from "@/context/landContext";
-
 import RoutesProtection from "@/hooks/routesProtection";
 
-
 export function Providers({ children }: { children: React.ReactNode }) {
-  // const redirectUri = process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI || "";
-  const redirectUri = 
-      process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI ||
+  const redirectUri =
+    process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI ||
     (typeof window !== "undefined" ? window.location.origin : "");
+
   return (
     <Auth0Provider
-    domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN!}
-    clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID!}
-    authorizationParams={{
-      redirect_uri: redirectUri,
-    }}
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN!}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID!}
+      authorizationParams={{
+        redirect_uri: redirectUri,
+      }}
     >
       <AuthProvider>
-
-       <LandProvider>
-        {children}
+        <LandProvider>
+          <RoutesProtection>
+            {children}
+          </RoutesProtection>
         </LandProvider>
-        </AuthProvider>
-
-      <RoutesProtection>
-    </RoutesProtection>
       </AuthProvider>
     </Auth0Provider>
   );
