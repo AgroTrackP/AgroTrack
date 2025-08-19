8fb3d6287d92a0cabf85021020c6e084e55c2516
FStevenson91 <felstefer28791@gmail.com>
2025-08-19 13:40:39 -0400

    resuelto conflicto user.entity, agregados seeders de application plans y types, ademas se incluye funcion runSeeders en main para q se ejecuten en orden correspondiente al inicializar el servidor, no se duplican elementos

--- back/src/Modules/ApplicationTypes/seeders/applicationtypes.seeder.ts
+++ back/src/Modules/ApplicationTypes/seeders/applicationtypes.seeder.ts
@@ -2,7 +2,8 @@ import { DataSource } from 'typeorm';
 import { connectionSource } from 'src/Config/TypeORM.config';
 import { ApplicationType } from '../entities/applicationtype.entity';
 import { Users } from 'src/Modules/Users/entities/user.entity';
-
+import * as fs from 'fs';
+import * as path from 'path';
 export class ApplicationTypesSeeder {
   static async run() {
     const ds: DataSource = connectionSource;
@@ -11,25 +12,20 @@ export class ApplicationTypesSeeder {
     const repo = ds.getRepository(ApplicationType);
     const userRepo = ds.getRepository(Users);
 
-    const user = await userRepo.findOneBy({ email: 'admin@demo.com' }); // ejemplo
+    const user = await userRepo.findOneBy({
+      email: 'facundo.ortiz@example.com',
+    }); // ejemplo
     if (!user) {
       console.error('⚠️ No se encontró usuario admin para asociar');
       await ds.destroy();
       return;
     }
+    const filePath = path.join(__dirname, '../data/applicationtypes.json');
+    const applicationtypesData: { name: string }[] = JSON.parse(
+      fs.readFileSync(filePath, 'utf-8'),
+    );
 
-    const types = [
-      {
-        name: 'Preventiva',
-        description: 'Aplicación antes de síntomas',
-      },
-      {
-        name: 'Curativa',
-        description: 'Aplicación tras aparición de síntomas',
-      },
-    ];
-
-    for (const t of types) {
+    for (const t of applicationtypesData) {
       const exists = await repo.findOne({ where: { name: t.name } });
       if (!exists) {
         const type = repo.create({ ...t, user });
--- back/src/Modules/seed.ts
+++ back/src/Modules/seed.ts
@@ -2,17 +2,22 @@ import 'dotenv/config';
 import { connectionSource } from 'src/Config/TypeORM.config';
 import { CategoriesSeeder } from './Categories/seeders/categories.seeder';
 import { ProductsSeeder } from './Products/seeders/products.seeder';
-async function runSeeders() {
+import { PlantationsSeeder } from './Plantations/seeders/plantations.seeder';
+import { PhenologiesSeeder } from './Phenologies/seeders/phenologies.seeder';
+import { DiseasesSeeder } from './Diseases/seeders/diseases.seeder';
+import { ApplicationTypesSeeder } from './ApplicationTypes/seeders/applicationtypes.seeder';
+import { ApplicationPlansSeeder } from './ApplicationPlans/seeders/applicationplans.seeder';
+
+export async function runSeeders() {
   await connectionSource.initialize();
 
   await CategoriesSeeder.run();
   await ProductsSeeder.run();
+  await PlantationsSeeder.run();
+  await PhenologiesSeeder.run();
+  await DiseasesSeeder.run();
+  await ApplicationTypesSeeder.run();
+  await ApplicationPlansSeeder.run();
 
   console.log('🎉 All seeders completed');
-  process.exit(0);
 }
-
-runSeeders().catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
