415d11c51921b875aa5f9c04a8971215aa7b9eef
FStevenson91 <felstefer28791@gmail.com>
2025-08-25 12:34:55 -0400

    servicio para consumir info que envia cliente en formulario de contacto

--- back/src/Modules/Contacto/contacto.controller.ts
+++ back/src/Modules/Contacto/contacto.controller.ts
@@ -1,6 +1,10 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
 import { ContactService } from './conctacto.service';
 import { CreateContactDto } from './dto/contacto.dto';
+import { PassportJwtAuthGuard } from 'src/Guards/passportJwt.guard';
+import { RoleGuard } from 'src/Guards/role.guard';
+import { Roles } from '../Auth/decorators/roles.decorator';
+import { Role } from '../Users/user.enum';
 
 @Controller('contact')
 export class ContactController {
@@ -9,5 +13,11 @@ export class ContactController {
   @Post()
   sendContact(@Body() dto: CreateContactDto) {
     return this.contactService.createContact(dto);
+  }
+  @UseGuards(PassportJwtAuthGuard, RoleGuard)
+  @Roles(Role.Admin)
+  @Get()
+  getAllContacts() {
+    return this.contactService.findAllContacts();
   }
 }
