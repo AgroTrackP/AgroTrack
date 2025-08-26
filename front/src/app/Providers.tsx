248f1bd10941fe762baacf0ca058cd9f77c0b01f
JeisonGuasca <guascajeison@gmail.com>
2025-08-26 14:55:49 -0500

    BotonesPerfil

--- front/src/app/Providers.tsx
+++ front/src/app/Providers.tsx
@@ -17,6 +17,8 @@ export function Providers({ children }: { children: React.ReactNode }) {
       clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID!}
       authorizationParams={{
         redirect_uri: redirectUri,
+        audience: process.env.NEXT_PUBLIC_AUTH0_AUDIENCE, 
+        scope: "openid profile email",
       }}
     >
       <AuthProvider>
